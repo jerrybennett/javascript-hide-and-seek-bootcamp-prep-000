81bd35602ca1d4f8807f0ddb78aeeb4fac130ff4
Jerry Bennett <jerrysbusiness@gmail.com>
2017-09-19 18:55:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,33 +16,15 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  const deepDive = document.getElementById('grand-node').querySelector('div')
+  const deepDive = document.getElementById('grand-node').querySelectorAll('div')
   const arr = []
-function find(array, criteriaFn) {
-  const current = deepDive
-  const next = []
-  while (current) {
-    if (criteriaFn(current)) {
-      return current
-    }
-    // if `current` is an array, we want to push all of
-    // its elements (which might be arrays) onto `next`
-    if (Array.isArray(current)) {
-      for (let i = 0; i < current.length; i++) {
-        next.push(current[i])
+  function find(array, criteriaFn) {
+    for (let i = 0; i < array.length; i++) {
+      if (criteriaFn(array[i])) {
+        return array[i]
       }
     }
-    // after pushing any children (if there
-    // are any) of `current` onto `next`, we want to take
-    // the first element of `next` and make it the
-    // new `current` for the next pass of the `while`
-    // loop
-    current = next.shift()
   }
-  // if we haven't
-  return null
-  }
-}
 }
 
 <div id="grand-node">
