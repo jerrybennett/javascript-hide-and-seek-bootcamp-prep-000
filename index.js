c799603aecb2dc5fcda05ea9054511881f036bd6
Jerry Bennett <jerrysbusiness@gmail.com>
2017-09-19 18:11:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function increaseRankBy(n) {
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
 
   for (let i = 0; i < lis.length; i++) {
-    parseInt(lis[i].innerHTML = (i + 1))
+    lis[i].innerHTML = (i + 1).toString()
   }
 }
