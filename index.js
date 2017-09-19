5494561db8d499ad7f81c9f4c46aa4c8dc1d552b
Jerry Bennett <jerrysbusiness@gmail.com>
2017-09-19 19:34:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return getFirstSelector('.target')
+}
+
+function increaseRankBy(n) {
+  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < list.length; i++) {
+    const start = parseInt(list[i].innerHTML)
+    list[i].innerHTML = (start + n)
+  }
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
