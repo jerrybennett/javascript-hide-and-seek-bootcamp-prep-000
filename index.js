503d02f05f5a9faae0cd3d850a364d33b4dbff06
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-03 15:27:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-function getFirstSelector(selector) {
-  return document.querySelector(selector)
+function getFirstSelector( selector ) {
+  return document.querySelector(selector);
 }
 
 function nestedTarget() {
-  return getFirstSelector('.target')
+  return getFirstSelector('.target');
 }
 
 function increaseRankBy(n) {
-  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li');
 
   for (let i = 0; i < list.length; i++) {
-    const start = parseInt(list[i].innerHTML)
-    list[i].innerHTML = (start + n)
+    const start = parseInt(list[i].innerHTML);
+    list[i].innerHTML = (start + n);
   }
 }
 
 function deepestChild() {
-  return document.querySelector('#grand-node div div div div')
+ // create array of all divs in #grand-node
+ const boo = document.querySelectorAll('#grand-node div');
+ // use spread syntax to iterate through array and return last div
+ return [...boo].pop();
 }
